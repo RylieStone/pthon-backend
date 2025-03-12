c652c640bfb839c97bcf860666a73596c9231365
Rylie <rylie.stone2005@gmail.com>
2025-03-12 08:19:12 -0400

    removed uneeded code

--- python.py
+++ python.py
@@ -1,5 +1,4 @@
 from math import *
-print("my calculater")
 friend_list = ["Rylie", "Matthew", "Jackson", "Roddy", "David", "Leo"]
 object = {"name": "rylie", "age": 19, "gender": "male", "friends": [1, 3, 4]}
 friend_list.remove("David")
