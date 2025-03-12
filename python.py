fcbd1ebec9de46af5f1b890bda6905d80dd9c7dc
Rylie <rylie.stone2005@gmail.com>
2025-03-12 08:14:49 -0400

    sort method added

--- python.py
+++ python.py
@@ -3,5 +3,6 @@ print("my calculater")
 friend_list = ["Rylie", "Matthew", "Jackson", "Roddy", "David", "Leo"]
 object = {"name": "rylie", "age": 19, "gender": "male", "friends": [1, 3, 4]}
 friend_list.remove("David")
+friend_list.sort()
 print(object)
 print(friend_list)
