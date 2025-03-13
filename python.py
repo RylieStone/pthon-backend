373557e5998b7e587c696c410a09c5b53d5eee5a
Rylie <rylie.stone2005@gmail.com>
2025-03-13 09:00:51 -0400

    advanced calc

--- python.py
+++ python.py
@@ -1,11 +1,15 @@
 from math import *
-friend_list = ["Rylie", "Matthew", "Jackson", "Roddy", "David", "Leo"]
-object = {"name": "rylie", "age": 19, "gender": "male", "friends": [1, 3, 4]}
-friend_list.remove("David")
-def friend_maker(friend_name, age, gender, friends) :
-    return {"name": friend_name, "age": age, "gender": gender, "friends": friends}
-friend = friend_maker("Matthew", 21, "male", [0, 2, 4])
-friend_list.sort()
-print(object)
-print(friend_list)
-print(friend)
+num1 = float(input("please input the first number: "))
+num2 = float(input("please input the second number: "))
+symbol = input("please input the symbol you'd like to use: ")
+
+if symbol == "plus" or symbol == "+":
+    print(num1 + num2)
+elif symbol == "multiply" or symbol == "*" or symbol == "x":
+    print(num1 * num2)
+elif symbol == "divide" or symbol == "/":
+    print(num1 / num2)
+elif symbol == "minus" or symbol == "-":
+    print(num1 - num2)
+else:
+    print("invalid operator, try again")
